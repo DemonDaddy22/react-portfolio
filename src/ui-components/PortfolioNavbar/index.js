9191f50993146d8083e9e820bf1bdd220818cc1c
Rohan Gupta <rohan.gupta2@oyorooms.com>
2020-03-28 17:38:09 +0530

    add nav bio

---
+++ src/ui-components/PortfolioNavbar/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Navbar, Nav } from 'react-bootstrap';
+import './index.css';
+
+export default class PortfolioNavbar extends React.Component {
+
+    render = () => {
+        return (
+            <Navbar className='pd-5' expand='md' variant={this.props.variant}>
+                <Navbar.Brand className='nav-title ml-2' href='#home'>Shades of Demon</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav' className='ml-auto' style={{ flexGrow: 'inherit' }}>
+                    <Nav>
+                        <Nav.Link className={`${this.props.linkColor} nav-item`} href='#link'>About</Nav.Link>
+                        <Nav.Link className={`${this.props.linkColor} nav-item`} href='#link'>Projects</Nav.Link>
+                        <Nav.Link className={`${this.props.linkColor} nav-item`} href='#link'>Gallery</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
