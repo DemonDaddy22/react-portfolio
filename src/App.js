aba69d063504e9bea96a4de944a98fb7c692e9a2
Rohan Gupta <rohan.gupta2@oyorooms.com>
2020-03-28 15:10:48 +0530

    add bootstrap and background

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+    render = () => {
+        return (
+            <div className='body-bg'>
+                <div className='body-container'>
+                    <Navbar className='navbar-bg navbar-top pd-5' expand="md" variant='dark'>
+                        <Navbar.Brand href="#home">SHADES OF DEMON</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav" className='ml-auto' style={{ flexGrow: 'inherit' }}>
+                            <Nav>
+                                <Nav.Link href="#link">About</Nav.Link>
+                                <Nav.Link href="#link">Projects</Nav.Link>
+                                <Nav.Link href="#link">Gallery</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default App;
