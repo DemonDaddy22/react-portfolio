4f66416243433b024b6da21f7a78630bd5c963ae
Rohan Gupta <rohan.gupta2@oyorooms.com>
2020-04-04 22:54:03 +0530

    add project row component

---
+++ src/modules/Portfolio/Projects/components/ProjectRow/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './index.css';
+import { Container, Row } from 'react-bootstrap';
+
+export default class ProjectRow extends React.Component {
+
+    render = () => {
+        return (
+            <div id={`row-${this.props.index}`} style={{ backgroundColor: this.props.bgColor, color: this.props.color }} className='project-row'>
+                <Container>
+                    <Row style={{marginBottom: '6rem'}} className='align-items-center'>
+                        <div className={`my-5 col-sm-12 col-md-5 order-2 order-md-${this.props.imgOrder}`}>
+                            <img src={this.props.img} alt={this.props.title.toLowerCase()} className='d-block mx-auto img-fluid project-img' />
+                        </div>
+                        <div className={`col-sm-12 col-md-7 text-center order-1 order-md-${this.props.contentOrder}`}>
+                            <Row className='justify-content-center'>
+                                <div className='col-10'>
+                                    <div className='project-title my-3'>{this.props.title}</div>
+                                    <ul className='project-content text-left'>
+                                    {this.props.content.map((entry, index) => {
+                                        return (
+                                            <li key={`content-${index}`}>{entry}</li>
+                                        )
+                                    })}
+                                    </ul>
+                                </div>
+                            </Row>
+                        </div>
+                    </Row>
+                </Container>
+                { this.props.index !==6 && <div className='text-center'><i className='fas fa-chevron-down next-section-btn'></i></div> }
+            </div>
+        );
+    }
+}
--- src/modules/Portfolio/Projects/index.js
+++ src/modules/Portfolio/Projects/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Page from '../../../ui-components/Page';
 import PortfolioNavbar from '../../../ui-components/PortfolioNavbar';
 import { Row, Col } from 'react-bootstrap';
+import ProjectRow from './components/ProjectRow';
 
 export default class extends React.Component {
 
@@ -30,10 +31,35 @@ export default class extends React.Component {
                         </Col>
                     </Row>
                 </div>
+                
+                <ProjectRow index={1} transitionDirection='left' bgColor='#FFFFFF' color='#5638A9' imgOrder={1} contentOrder={2} title={'YelpCamp'} 
+                img='https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+                content={[
+                    'An interactive and responsive website that allows users to create a profile, add campgrounds or view campgrounds added by others and also interact with them by posting comments and reviews.',
+                    'The UI of this project has been developed using HTML, CSS, JavaScript and back-end has been done using NodeJS, Express and MongoDB.',
+                    ''
+                ]} />
+                
+                <ProjectRow index={2} transitionDirection='right' bgColor='#5638A9' color='#FFFFFF' imgOrder={2} contentOrder={1} title={'YelpCamp'} 
+                img='https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+                content={['tales better twice poem somehow never leaving forgotten pattern observe chicken neck shine silence folks eaten keep strip class clock run list scared globe', 'chapter opportunity difficulty note care believed team friend friendly cutting kitchen driver additional settlers blow gulf mostly eventually rock toy rate hide jet sand', 'tax picture tales nuts under had uncle source noted drawn heading rough me beneath stiff degree palace addition took major colony fellow moving of']} />
+                
+                <ProjectRow index={3} transitionDirection='left' bgColor='#FFFFFF' color='#5638A9' imgOrder={1} contentOrder={2} title={'YelpCamp'} 
+                img='https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+                content={['needed thank yourself calm scared donkey noted alone simple horse heat surrounded sink wind hope worker place scientific winter kitchen thin agree connected possibly', 'nose printed thrown would mile youth aside arrow battle sudden able necessary create beneath thing sum school hidden huge forget electricity as dull gun', 'possibly few happen religious pack slowly great truck shore laid studying hunter excited doll planned another shout author rise unusual red stomach roar dry']} />
+                
+                <ProjectRow index={4} transitionDirection='right' bgColor='#5638A9' color='#FFFFFF' imgOrder={2} contentOrder={1} title={'YelpCamp'} 
+                img='https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+                content={['general arm seems sharp such gone ate baby distant rice please bigger length suggest price doctor sense monkey grandfather dried equal temperature cross mathematics', 'giving slave excitement triangle begun damage studying thank brick them sign nervous full music gate now ear quiet grow syllable neighborhood page arrow bend', 'car drew zoo sure apartment share stopped city wood changing lesson master mission win golden system point shallow hide deer cup hearing medicine finish']} />
+                
+                <ProjectRow index={5} transitionDirection='left' bgColor='#FFFFFF' color='#5638A9' imgOrder={1} contentOrder={2} title={'YelpCamp'} 
+                img='https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+                content={['belong parts four prove popular something wing facing run next topic diagram purpose review toward search develop brass cave policeman breakfast news inside gradually', 'air plastic else tail thou teeth beat helpful lose trail statement express knew gone making drop belt strike plan train available fact string willing', 'due dress they modern back affect making gulf cowboy anywhere cause entire larger wherever camera door product whatever wonderful announced instrument did trade industrial']} />
+                
+                <ProjectRow index={6} transitionDirection='right' bgColor='#5638A9' color='#FFFFFF' imgOrder={2} contentOrder={1} title={'YelpCamp'} 
+                img='https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+                content={['shop discussion recall however several herd effort tropical why box student shall noise buy north doubt dig chance represent separate support certain time size', 'outline immediately thought red shape warm worker since right jar want mission weight brown whether aware border sheet bean plain outer using bottle find', 'tone pot flew opinion beneath dream pattern alphabet like tax driving earn dead mirror fort minute upper shade mice law teacher any finally late']} />
 
-                {/* make separate component for each project row (specify colors, content, image, order at smaller screens),
-                height of each row should be equal to 100%vh
-                add down button to each row */}
             </Page>
         );
     }
