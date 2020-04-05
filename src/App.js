fb814ca1ea16d46bf409f0596e5a80965888874a
Rohan Gupta <rohan.gupta2@oyorooms.com>
2020-04-05 20:27:52 +0530

    test polyfill smoothscroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import smoothscroll from 'smoothscroll-polyfill';
+
+smoothscroll.polyfill();
 
 export default class App extends React.Component {
 
